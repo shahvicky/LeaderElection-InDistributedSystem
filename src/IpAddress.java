b13c366f7c988a4d3b50c364edf48667f2189f16
prabhat <prabhuyesjha@gmail.com>
2018-03-09 11:53:53 -0600

    updating file

--- src/IpAddress.java
+++ src/IpAddress.java
@@ -1,4 +1,13 @@
-
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 public class IpAddress {
-
+  public static void main(String args[]){
+    try {
+      InetAddress inetAddress = InetAddress.getLocalHost();
+      System.out.println("IP Address: "+inetAddress.getHostAddress());
+      System.out.println("Hostname: "+inetAddress.getHostName());
+    }catch(UnknownHostException unknownHostException){
+      unknownHostException.printStackTrace();
+    }
+  }
 }
